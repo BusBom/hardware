b5e8eb5833b2f4fbb6f13c866495f706620f9bc0
yschoi <youngSeok0413@gmail.com>
2025-07-22 16:41:02 +0100

    driver protoype

--- uart/driver/serdev_uart.c
+++ uart/driver/serdev_uart.c
@@ -6,6 +6,8 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/hrtimer.h>
+#include <linux/ktime.h>
 
 #define DEVICE_NAME "serdev-uart"
 
@@ -14,9 +16,38 @@ MODULE_AUTHOR("Raspberry Pi Developer");
 MODULE_DESCRIPTION("UART serdev driver with string I/O via /dev/serdev-uart");
 
 static struct serdev_device *global_serdev;
+
+/*uart */
 static char rx_buffer[256];
 static bool is_updated = true;
 static size_t rx_size;
+
+/*timer */
+static struct hrtimer main_timer; //timer for sending command
+static ktime_t main_period;
+static struct hrtimer sub_timer; //timer for clock and on/off 
+static ktime_t sub_period;
+
+/*main timer callback*/
+static enum hrtimer_restart main_timer_callback(struct hrtimer *timer){
+	
+	printk("main timer");
+	
+	hrtimer_forward_now(timer, main_period);  // 주기 설정
+	return HRTIMER_RESTART;
+}
+
+/*sub timer callback*/
+
+static bool device_state = false; // true : on, false : off
+
+static enum hrtimer_restart sub_timer_callback(struct hrtimer *timer){
+	
+	printk("sub timer");
+
+	hrtimer_forward_now(timer, sub_period);  // 주기 설정
+	return HRTIMER_RESTART;
+}
 
 /* Receive callback from UART */
 static int serdev_uart_recv(struct serdev_device *serdev, const unsigned char *buffer, size_t size) {
@@ -36,6 +67,7 @@ static int serdev_uart_recv(struct serdev_device *serdev, const unsigned char *b
 static const struct serdev_device_ops serdev_echo_ops = {
 	.receive_buf = serdev_uart_recv,
 };
+
 
 /* Character device: read from rx_buffer */
 static ssize_t dev_read(struct file *file, char __user *buf, size_t count, loff_t *ppos) {
@@ -100,9 +132,21 @@ static int serdev_uart_probe(struct serdev_device *serdev) {
 		return ret;
 	}
 
+	//uart driver
 	serdev_device_set_baudrate(serdev, 115200);
 	serdev_device_set_flow_control(serdev, false);
 	serdev_device_set_parity(serdev, SERDEV_PARITY_NONE);
+
+	main_period = ktime_set(1, 0);
+	sub_period = ktime_set(2, 0);
+
+	hrtimer_init(&main_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    main_timer.function = main_timer_callback;
+    hrtimer_start(&main_timer, main_period, HRTIMER_MODE_REL);
+
+	hrtimer_init(&sub_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    sub_timer.function = sub_timer_callback;
+    hrtimer_start(&sub_timer, sub_period, HRTIMER_MODE_REL);
 
 	ret = misc_register(&echo_miscdev);
 	if (ret) {
@@ -117,6 +161,10 @@ static int serdev_uart_probe(struct serdev_device *serdev) {
 
 static void serdev_uart_remove(struct serdev_device *serdev) {
 	pr_info("serdev_echo: remove called\n");
+
+	hrtimer_cancel(&main_timer);
+    hrtimer_cancel(&sub_timer);
+
 	misc_deregister(&echo_miscdev);
 	serdev_device_close(serdev);
 	global_serdev = NULL;
